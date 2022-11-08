9a43e64b53cdf34d8e752197239186b6cbf267c0
juan <juanayala@gmail.com>
2022-11-08 14:31:09 -0300

    add dependencies and connect db

---
+++ src/routes/products.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const mysqlConnection = require("../database");
+
+
+router.get("/", (req, res)=>{
+    mysqlConnection.query("SELECT * FROM product", (err, rows, fields)=>{
+        if(!err){
+            res.json(rows);
+        } else{
+            console.error(err)
+        }
+    });
+})
+
+module.exports = router;
